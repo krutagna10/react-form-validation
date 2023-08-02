1cb99fe23445c791c768eacd98e229018c8e93c5
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-08-02 14:30:58 +0530

    final commit

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import useInput from "../../hooks/useInput";
 import "./Form.css";
 
--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const INITIAL_STATE = {
+  value: "",
+  isTouched: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "value-change": {
+      return { ...state, value: action.value };
+    }
+    case "is-touched-change": {
+      return { ...state, isTouched: action.isTouched };
+    }
+  }
+}
 
 function useInput(validateValue) {
-  const [value, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
-
-  const isValueValid = validateValue(value);
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   function handleValueChange(value) {
-    setEnteredValue(value);
+    dispatch({ type: "value-change", value: value });
   }
 
   function handleIsTouchedChange(isTouched) {
-    setIsTouched(isTouched);
+    dispatch({ type: "is-touched-change", isTouched: isTouched });
   }
 
+  const isValueValid = validateValue(state.value);
+
   return {
-    value: value,
+    value: state.value,
     isValueValid: isValueValid,
-    isTouched: isTouched,
+    isTouched: state.isTouched,
     handleValueChange: handleValueChange,
     handleIsTouchedChange: handleIsTouchedChange,
   };
